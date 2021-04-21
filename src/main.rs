45da29355a3ec1703235cc1a136145880cb450dd
HydroZA <=>
2021-04-21 15:19:04 +0100

    move networking to seperate module for readability

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use std::net::{Shutdown, TcpListener, TcpStream};
-use std::io::prelude::*;
-use std::io::{Error, ErrorKind};
 use std::thread;
 use std::str;
-use std::collections::HashMap;
 use std::time::SystemTime;
 use maplit::hashmap;
 use chrono::DateTime;
@@ -16,6 +13,9 @@ use mysql::*;
 use mysql::prelude::*;
 //use serde_json::json;
 
+// Import the Messaging module
+mod networking;
+
 #[derive(Serialize, Deserialize, Debug)]
 struct LoginData {
     client_token: String,
@@ -24,114 +24,7 @@ struct LoginData {
     username: String
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-enum MessageType {
-    SendToken,
-    RequestToken,
-    Username,
-    LoginRequest,
-    Result(OperationResult)
-}
 
-#[derive(Serialize, Deserialize, Debug)]
-enum OperationResult {
-    Success,
-    Fail
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Message {
-    header: HashMap<String, String>,
-    msg_type: MessageType,
-    body: Option<HashMap<String, String>>
-}
-
-fn remove_leading_zeroes(s: &str) -> &str {
-    let mut out = "";
-    for (i, c) in s.chars().enumerate() {
-        if c == '0' {
-            out = &s[i..];
-        }
-        else {
-            break;
-        }
-    }
-    out
-}
-
-fn read_message(stream: &mut TcpStream) -> std::result::Result<Message, serde_json::Error> {
-    // Read 1 byte representing message length
-    let mut len_buf = vec![0u8; 4];
-    stream.read_exact(&mut len_buf).expect("Unable to read message length");
-
-    // Convert the received byte to a usize
-    let len: &str = str::from_utf8(&len_buf[..])
-        .unwrap()
-        .trim();
-    
-    let len: usize = remove_leading_zeroes(len)
-        .parse()
-        .unwrap();
-
-    println!("Reading {} bytes", len);
-    
-        // Read the specified amount of bytes from the stream
-    let mut msg_buf = vec![0u8; len];
-    stream.read_exact(&mut msg_buf).expect("Unable to read message");
-    
-    println!("Got all bytes");
-
-    // Convert the received bytes into a string
-    let msg = match str::from_utf8(&msg_buf[..]) {
-        Ok(msg) => msg,
-        Err(_) => panic! ("Unable to convert message to string")
-    };
-
-    // Flush the stream
-    stream.flush().expect("Unable to flush stream");
-
-    // Parse the Json string into a Message object
-    serde_json::from_str(&msg)
-
-   
-}
-
-impl Message {
-    // On the server side we will only ever have to retrieve the client token
-    fn get_body_value(&self, key: &str) -> String {
-        self.body
-            .as_ref()
-            .unwrap()
-            .get(key)
-            .expect("Unable to find key")
-            .to_string()
-    }
-
-    fn send(&self, stream: &mut TcpStream) -> std::io::Result<()> {
-        // convert self into json
-        // we exit the function if this fails
-        let json = match serde_json::to_string(self) {
-            Ok(s) => s,
-            Err(e) => 
-                return Err(Error::new(ErrorKind::Other, e))
-        };
-
-        // Convert the json into bytes
-        let serialized = json.as_bytes();
-
-        // Write the length of the message first so the recipient knows how many bytes to listen for
-        let len = serialized.len().to_string();
-
-        // pad len to always be 4 bytes long
-        let len: String = format!("{:0>4}", len);
-
- //       println!("{}", len);
-        
-        stream.write(len.as_bytes()).expect("Unable to write message length");
-
-        stream.write_all(serialized)
-    }
-}
 fn gen_token() -> String {
     return thread_rng()
         .sample_iter(&Alphanumeric)
@@ -173,11 +66,11 @@ fn get_server_secret(username: &str) -> String {
     The server checks if sha( T1 + T2 + < password: server's copy > ) matches M (the hash just received).
 */
 
-fn process_login_request(data: &LoginData) -> MessageType {
+fn process_login_request(data: &LoginData) -> networking::MessageType {
     if data.username.is_empty() ||
         data.client_token.is_empty() ||
         data.client_hash.is_empty() {
-        return MessageType::Result(OperationResult::Fail);
+        return networking::MessageType::Result(networking::OperationResult::Fail);
     }
 
     println!("Processing login request for: {:?}", data);
@@ -189,21 +82,21 @@ fn process_login_request(data: &LoginData) -> MessageType {
     );
     let server_hash: String = digest(server_hash);
     if server_hash == data.client_hash {
-        return MessageType::Result(OperationResult::Success);
+        return networking::MessageType::Result(networking::OperationResult::Success);
     }
     else {
-        return MessageType::Result(OperationResult::Fail);
+        return networking::MessageType::Result(networking::OperationResult::Fail);
     }
 }
 
 #[allow(unreachable_code)]
 fn handle_client(mut stream: TcpStream) {
     // Let the client know they're connected
-    let msg_connected = Message {
+    let msg_connected = networking::Message {
         header: hashmap!{
             String::from("timestamp") => get_timestamp()
         },
-        msg_type: MessageType::Result(OperationResult::Success),
+        msg_type: networking::MessageType::Result(networking::OperationResult::Success),
         body: None
     };
 
@@ -216,32 +109,34 @@ fn handle_client(mut stream: TcpStream) {
         client_hash: String::new()
     };
 
+
     loop {
-        let msg: Message = read_message(&mut stream)
+        let msg: networking::Message = 
+            networking::Message::read_into_message(&mut stream)
             .expect("Unable to read message");
 
         println!("Message type: {:?}", msg.msg_type);
 
         match msg.msg_type {
-            MessageType::SendToken => 
+            networking::MessageType::SendToken => 
                 login_data.client_token = msg.get_body_value("token"),
-            MessageType::RequestToken => {
-                Message {
+            networking::MessageType::RequestToken => {
+                networking::Message {
                     header: hashmap! {
                         String::from("timestamp") => get_timestamp()
                     },
-                    msg_type: MessageType::SendToken,
+                    msg_type: networking::MessageType::SendToken,
                     body: Some(hashmap! {
                         String::from("token") => login_data.server_token.clone()
                     })
                 }.send(&mut stream).expect("Unable to send token")
             },
-            MessageType::Username => 
+            networking::MessageType::Username => 
                 login_data.username = msg.get_body_value("username"),
-            MessageType::LoginRequest => {
+                networking::MessageType::LoginRequest => {
                 login_data.client_hash = msg.get_body_value("client_hash");
 
-                Message {
+                networking::Message {
                     header: hashmap! {
                         String::from("timestamp") => get_timestamp()
                     },
@@ -249,7 +144,7 @@ fn handle_client(mut stream: TcpStream) {
                     body: None
                 }.send(&mut stream).expect("Unable to send login result")
             },
-            MessageType::Result(_r) => 
+            networking::MessageType::Result(_r) => 
                 panic!("Not Yet Implemented")
         };
     }
